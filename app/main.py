f89806af3ea00f52b92a9ac4fca70fb126ce1430
Dadarzz2405 <haidaralifawwaz@gmail.com>
2026-02-11 08:26:02 +0700

    init

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title="My FastAPI Application",
+    description="This is a sample FastAPI application with CORS middleware.",
+    version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
